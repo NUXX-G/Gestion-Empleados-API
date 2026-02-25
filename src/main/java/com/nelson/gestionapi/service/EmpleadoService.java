68c8ab6912d23b93c28081f6b1c7f3d2763491ba
NUXX-G <nelsonffkarlsson.com>
2026-02-25 20:55:14 +0100

    empleadoService

--- src/main/java/com/nelson/gestionapi/service/EmpleadoService.java
+++ src/main/java/com/nelson/gestionapi/service/EmpleadoService.java
@@ -1,8 +1,10 @@
 package com.nelson.gestionapi.service;
 
+import com.nelson.gestionapi.model.Empleado;
 import com.nelson.gestionapi.repository.EmpleadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 
@@ -11,8 +13,36 @@ public class EmpleadoService
     @Autowired
     private EmpleadoRepository empleadoRepository;
 
-    public void listarEmpleados()
+    public List<Empleado> listarEmpleados()
     {
-        
+        return empleadoRepository.findAll();
+    }
+
+    public Empleado guardarEmpleado(Empleado empleado)
+    {
+        return empleadoRepository.save(empleado);
+    }
+
+    public Empleado obtenerEmpleado(Long id)
+    {
+        return empleadoRepository.findById(id).orElseThrow();
+    }
+
+    public void eliminarEmpleado(Long id)
+    {
+        empleadoRepository.deleteById(id);
+    }
+
+    public Empleado actualizarEmpleado(Long id, Empleado empleado)
+    {
+        Empleado empleadoAct = obtenerEmpleado(id);
+        empleadoAct.setApellido(empleado.getApellido());
+        empleadoAct.setDepartamento(empleado.getDepartamento());
+        empleadoAct.setEmail(empleado.getEmail());
+        empleadoAct.setNombre(empleado.getNombre());
+        empleadoAct.setSalario(empleado.getSalario());
+        guardarEmpleado(empleadoAct);
+
+        return empleadoAct;
     }
 }
